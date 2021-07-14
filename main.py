7caed0a4178c0001d5a8d7a136186d3ea3668bd3
Behzad Shomali <behzad@Behzads-MacBook-Pro.local>
2021-07-14 20:54:06 +0430

    Added "add/remove user" methods Added logs inserting statements during taking any action

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ if __name__ == '__main__':
     password = input('Enter your password: ')
     conn = connect(password=password)
 
-    add_defineing_image(
-        conn,
-        'behzad.shomali@gmail.com',
-        'https://drive.google.com/file/d/1arCk-433DmaHv46wtlBd3xnmQb4iTp0A/view?usp=sharing',
-        'Anna de Armas'
-    )
-    remove_defineing_image(
-        conn,
-        'behzad.shomali@gmail.com',
-        'https://drive.google.com/file/d/1arCk-433DmaHv46wtlBd3xnmQb4iTp0A/view?usp=sharing',
-    )
-
+    # add_defining_image(
+    #     conn,
+    #     'behnam.shomali@gmail.com',
+    #     'https://drive.google.com/file/d/1arCk-433DmaHv46wtlBd3xnmQb4iTp0A/view?usp=sharing',
+    #     'Anna de Armas'
+    # )
+    # remove_defining_image(
+    #     conn,
+    #     'behzad.shomali@gmail.com',
+    #     'https://drive.google.com/file/d/1-m7nsPQxdyx4yxIWhC4KaatUkyk_YiGX/view?usp=sharing',
+    # )
+    add_user(conn, 'Behnam Shomali', 15, 'behnam.shomali@gmail.com', '123')
+    # remove_user(conn, 'behnam.shomali@gmail.com')
     conn.close()
-    print('Databse connection closed.')
+    print('Database connection closed.')
--- postgres.py
+++ postgres.py
@@ -16,7 +16,7 @@ def connect(password, host='localhost', database='Image describe pipe DB', user=
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
-def add_defineing_image(conn, user_email, image_url, who_is_in):
+def add_defining_image(conn, user_email, image_url, who_is_in):
     try:
         cur = conn.cursor()
         cur.execute(
@@ -27,7 +27,14 @@ def add_defineing_image(conn, user_email, image_url, who_is_in):
                 '{image_url}',
                 '{who_is_in}'
             )
-            ON CONFLICT (image_url, user_email) DO UPDATE SET who_is_in = EXCLUDED.who_is_in
+            ON CONFLICT (image_url, user_email) DO UPDATE SET who_is_in = EXCLUDED.who_is_in;
+
+            INSERT INTO public.logs(user_email, action, date)
+            VALUES(
+                '{user_email}',
+                'Added/updated a defining image',
+                NOW()
+            )
             '''
         )
         conn.commit()
@@ -37,7 +44,7 @@ def add_defineing_image(conn, user_email, image_url, who_is_in):
         print(error)
 
 
-def remove_defineing_image(conn, user_email, image_url):
+def remove_defining_image(conn, user_email, image_url):
     try:
         cur = conn.cursor()
         cur.execute(
@@ -49,11 +56,102 @@ def remove_defineing_image(conn, user_email, image_url):
         )
         images_count = len(cur.fetchall())
         if images_count == 1:
-            conn.commit()
+            cur.execute(
+                f'''
+                INSERT INTO public.logs(user_email, action, date)
+                VALUES(
+                    '{user_email}',
+                    'Removed a defining image',
+                    NOW()
+                )
+                '''
+            )
             print('The intended image has been removed from database successfully.')
         else:
+            cur.execute(
+                f'''
+                INSERT INTO public.logs(user_email, action, date)
+                VALUES(
+                    '{user_email}',
+                    'Tried to remove a unexisting defining image',
+                    NOW()
+                )
+                '''
+            )
             print('There is no image with the input information in the database.')
         
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def add_user(conn, full_name, age, email, password):
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            f'''
+            INSERT INTO public.users(full_name, age, email, password)
+            VALUES(
+                '{full_name}',
+                {age},
+                '{email}',
+                '{password}'
+            )
+            ON CONFLICT (email) DO UPDATE SET full_name = EXCLUDED.full_name, age = EXCLUDED.age, password = EXCLUDED.password;
+
+            INSERT INTO public.logs(user_email, action, date)
+            VALUES(
+                '{email}',
+                'User is updated/added to database',
+                NOW()
+            )
+            '''
+        )
+        conn.commit()
+        cur.close()
+        print('User has been added to database successfully.')
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def remove_user(conn, email):
+    try:
+        cur = conn.cursor()
+        cur.execute(
+            f'''
+            DELETE FROM public.users
+            WHERE email = '{email}'
+            RETURNING *
+            '''
+        )
+        users_count = len(cur.fetchall())
+        if users_count == 1:
+            # cur.execute(
+            #     f'''
+            #     INSERT INTO public.logs(user_email, action, date)
+            #     VALUES(
+            #         '{email}',
+            #         'Removed the user account',
+            #         NOW()
+            #     )
+            #     '''
+            # )
+            print('The intended user has been removed from database successfully.')
+        else:
+            # cur.execute(
+            #     f'''
+            #     INSERT INTO public.logs(user_email, action, date)
+            #     VALUES(
+            #         '{email}',
+            #         'Tried to remove a nonexisting user',
+            #         NOW()
+            #     )
+            #     '''
+            # )
+            print('There is no user with the input information in the database.')
+        
+        conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
