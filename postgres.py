243ea88f17c72c92a769fbcea517c863ba30ac24
Behzad Shomali <behzad.shomali@gmail.com>
2021-07-23 21:36:34 +0430

    Add comments to describe functions

--- postgres.py
+++ postgres.py
@@ -13,6 +13,7 @@ import cv2
 
 def connect(password, host='localhost', database='Image describe pipe DB', user='postgres'):
     """ Connect to the PostgreSQL database server """
+
     conn = None
     try:
         print('Connecting to the PostgreSQL database...')
@@ -29,26 +30,37 @@ def connect(password, host='localhost', database='Image describe pipe DB', user=
 
 
 def disconnect(conn):
+    """ Disconnect the PostgreSQL database server connection """
+
     conn.close()
     print('Database connection closed.')
 
 
 def add_defining_image(conn, user_email, image_url, who_is_in, model=DeepFace.build_model('Facenet512')):
-    os.system('mkdir ./tmp/')
-    # os.system(f'touch ./tmp/{who_is_in.split()[0]}.jpg')
+    ''' Add image to database which is used for evaluting the further images '''
 
-    # urllib.urlretrieve(
-    #     image_url
-    #     ,f'./tmp/{who_is_in.split()[0]}.jpg'
-    #     )
+    # Making a temp directory which is used for 
+    # storing the downloaded images
+    os.system('mkdir ./tmp/')
     try:
+        # Download the "defining_image" from its 
+        # corresponding URL
         with open(f'./tmp/{who_is_in.split()[0]}.jpg', 'wb') as f:
             f.write(requests.get(image_url).content)
 
+        # Read the downloaded into array of numbers
         img = plt.imread(f'./tmp/{who_is_in.split()[0]}.jpg')
+
+        # Get the corresponding embeding (vector of
+        # 512 numbers) of the image using a 
+        # pretrained model
         data = DeepFace.represent(img, 'Facenet512', model)
 
         cur = conn.cursor()
+
+        # Run the query which insert a new image
+        # to table "images" of the database and 
+        # also records its corresponding logs
         cur.execute(
             f'''
             INSERT INTO public.images(user_email, image_url, representation, who_is_in)
@@ -68,18 +80,28 @@ def add_defining_image(conn, user_email, image_url, who_is_in, model=DeepFace.bu
             )
             '''
         )
+
+        # Permanently store the applied changes
+        # in the database 
         conn.commit()
         cur.close()
         print('Image has been added to database successfully.')
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
+        # Remove the made temp directory
+        # which is unused anymore
         os.system(f'rm -rf ./tmp/')
 
 
 def remove_defining_image(conn, user_email, image_url):
+    ''' Remove an image from the database which was used for evaluating images '''
+
     try:
         cur = conn.cursor()
+
+        # Remove the intended image from the
+        # table "images" 
         cur.execute(
             f'''
             DELETE FROM public.images
@@ -87,8 +109,15 @@ def remove_defining_image(conn, user_email, image_url):
             RETURNING *
             '''
         )
+
+        # Get the number of affected rows 
+        # (deleted images)
         images_count = len(cur.fetchall())
+
+        # Check whether the intended URL has
+        # existed in the database
         if images_count == 1:
+            # Commit the corresponding logs
             cur.execute(
                 f'''
                 INSERT INTO public.logs(user_email, action, date)
@@ -101,6 +130,7 @@ def remove_defining_image(conn, user_email, image_url):
             )
             print('The intended image has been removed from database successfully.')
         else:
+            # Commit the corresponding logs
             cur.execute(
                 f'''
                 INSERT INTO public.logs(user_email, action, date)
@@ -113,6 +143,8 @@ def remove_defining_image(conn, user_email, image_url):
             )
             print('There is no image with the input information in the database.')
         
+        # Permanently store the applied changes
+        # in the database 
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -120,8 +152,13 @@ def remove_defining_image(conn, user_email, image_url):
 
 
 def add_user(conn, full_name, age, email, password):
+    ''' Add a new user to the database '''
     try:
         cur = conn.cursor()
+
+        # Run the query which insert a new 
+        # user to table "users" and also 
+        # records its corresponding logs
         cur.execute(
             f'''
             INSERT INTO public.users(full_name, age, email, password)
@@ -145,6 +182,9 @@ def add_user(conn, full_name, age, email, password):
                 NOW()
             '''
         )
+
+        # Permanently store the applied changes
+        # in the database 
         conn.commit()
         cur.close()
         print('User has been added to database successfully.')
@@ -153,8 +193,13 @@ def add_user(conn, full_name, age, email, password):
 
 
 def remove_user(conn, email):
+    ''' Remove a user from the database '''
+    
     try:
         cur = conn.cursor()
+        
+        # Run the query which removes the
+        # intended user from the database
         cur.execute(
             f'''
             DELETE FROM public.users
@@ -188,6 +233,8 @@ def remove_user(conn, email):
             # )
             print('There is no user with the input information in the database.')
         
+        # Permanently store the applied changes
+        # in the database 
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -195,17 +242,42 @@ def remove_user(conn, email):
 
 
 def evaluate_image(conn, user_email, image_url, model=DeepFace.build_model('Facenet512')):
+    '''
+    Evaluate the input image by gathering
+    all the images belong to a specific user
+    and the starts to get the Cosine-distance
+    between the input image and the user's
+    stored defining-images
+    '''
+    
+    # Specify the upperbund distance between two
+    # images' vector which are considered as belong
+    # to a same a person
     THRESHOLD = 0.6
+
+    # Making two directories. "tmp" which is used
+    # for storing the downloaded images and "output"
+    # which is used for storing the output of the function
     os.system('mkdir ./tmp/ ./tmp/DB ./output/')
 
     try:
+
+        # Download the "defining_image" from its 
+        # corresponding URL
         with open('./tmp/img.jpg', 'wb') as f:
             f.write(requests.get(image_url).content)
 
+        # Read the downloaded into array of numbers
         img = plt.imread('./tmp/img.jpg')
+
+        # Detect the faces appearing in the image
+        # by a confidence ratio of 95%
         faces = RetinaFace.detect_faces(img, 0.95)
 
         cur = conn.cursor()
+
+        # Run a query which collects all the images
+        # belong to the intended person
         cur.execute(
             f'''
             SELECT who_is_in, representation 
@@ -213,6 +285,9 @@ def evaluate_image(conn, user_email, image_url, model=DeepFace.build_model('Face
             WHERE user_email = '{user_email}'
             '''
         )
+
+        # Extract and preprocess the stored representation
+        # for each image
         representations_facenet = {}
         fetched_data = cur.fetchall()
         for indx, (who_is_in, representation) in enumerate(fetched_data):
@@ -222,16 +297,28 @@ def evaluate_image(conn, user_email, image_url, model=DeepFace.build_model('Face
             rep = list(map(float, rep.split(',')))
             representations_facenet[f'{who_is_in}_{indx}'] = rep
         
+        # Mark and recognize each face which is detected in the image
         for indx, face_info in enumerate(faces.values()):
             facial_area = face_info['facial_area']
             y1 = max(facial_area[1]-200, 0)
             y2 = min(facial_area[3]+200, img.shape[0]-1)
             x1 = max(facial_area[0]-50, 0)
             x2 = min(facial_area[2]+50, img.shape[1]-1)
+
+            # Cropped face extracted from the main image
             face = img[y1:y2, x1:x2]
+
+            # Save each extracted person seperately
             plt.imsave(f'./output/face{indx}.jpg', face)
             
+            # Get the corresponding embeding (vector of
+            # 512 numbers) of the extracted face using a 
+            # pretrained model
             img_rep = DeepFace.represent(np.asarray(face), 'Facenet512', model, False)
+
+            # Get the Cosine-distance between
+            # each extracted face and the user's
+            # stored defining-images
             for person in representations_facenet.keys():
                 distance = cosine(representations_facenet[person], img_rep)
                 print(person, distance)
@@ -245,9 +332,13 @@ def evaluate_image(conn, user_email, image_url, model=DeepFace.build_model('Face
 
                     break
         
+        # Store the output in which faces are 
+        # marked and recognized
         plt.imsave('./output/output.jpg', img)
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
+        # Remove the made temp directory
+        # which is unused anymore
         os.system(f'rm -rf ./tmp/')
