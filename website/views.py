49cee10476a43f1a07144861b1d722f0b2b388cc
Behzad Shomali <behzad.shomali@gmail.com>
2021-08-21 00:02:17 +0430

    Connect the backend/databse to frontend of the "Add new image" page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
+from postgres import connect, add_defining_image
+from .models import users
 
 views = Blueprint('views', __name__)
+conn = connect('123')
+
 
 @views.route('/')
 @views.route('/home')
@@ -17,6 +21,15 @@ def profile():
 
 
 @views.route('/home/addImage', methods=['GET', 'POST'])
+@views.route('/addImage', methods=['GET', 'POST'])
 @login_required
 def add_image():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        person = request.form.get('person')
+
+        add_defining_image(conn, user_email=current_user.email,
+                           image_url=url, who_is_in=person)
+        flash('Image has been added to database successfully.', category='success')
+
     return render_template('add_image.html', user=current_user)
