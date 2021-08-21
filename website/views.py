de2c9779abbd3b3cebc6cf7a4e92253d7b0f8f17
Behzad Shomali <behzad.shomali@gmail.com>
2021-08-21 16:06:25 +0430

    Image evaluation backend connected to frontend

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-from postgres import connect, add_defining_image
-from .models import users
+from postgres import connect, add_defining_image, evaluate_image
+import os
+
 
 views = Blueprint('views', __name__)
 conn = connect('123')
@@ -33,3 +34,44 @@ def add_image():
         flash('Image has been added to database successfully.', category='success')
 
     return render_template('add_image.html', user=current_user)
+
+
+@views.route('/home/describeImage', methods=['GET', 'POST'])
+@views.route('/describeImage', methods=['GET', 'POST'])
+@login_required
+def describe_image():
+    if request.method == 'POST':
+        url = request.form.get('url')
+
+        scene_description = ''
+        evaluate_image(conn, user_email=current_user.email, image_url=url)
+        with open("output/scene_output.txt") as f:
+            for line in f:
+                scene_description += line
+
+        emotions = ''
+        with open("output/emotions_output.txt") as f:
+            for line in f:
+                emotions += line
+                emotions += ', '
+            emotions = emotions[:-2]
+            emotions += '!'
+
+        os.system('rm -r output/')
+        
+        flash('Image has been added to database successfully.', category='success')
+
+        return render_template('describe_image.html',
+                               user=current_user,
+                               url=url, display='none',
+                               table_dis='none',
+                               res_table_dis='inline-table',
+                               scene_description=scene_description,
+                               emotions=emotions)
+    return render_template('describe_image.html',
+                           user=current_user,
+                           display='block',
+                           table_dis='inline-table',
+                           res_table_dis='none',
+                           scene_description='',
+                           emotions='')
